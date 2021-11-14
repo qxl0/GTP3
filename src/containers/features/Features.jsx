de34372b944c2ccb38cbdd72bbc9c3c183dfee97
Qiang <qxl0@yahoo.com>
2021-11-14 11:17:32 -0500

    set up folder structure

---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './features.css'
+
+
+const Features = () => {
+  return (
+    <div>
+      <h1>Features</h1>
+    </div>
+  )
+}
+
+export default Features
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './whatGPT3.css'
+
+const WhatGPT3 = () => {
+  return (
+    <div>
+      <h1>What is GPT-3?</h1>
+    </div>
+  )
+}
+
+export default WhatGPT3
